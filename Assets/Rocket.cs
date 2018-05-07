4018a3288b1434ecfbec2c47cae033451019f88f
Klim Agarkov <jewelgatorstv@gmail.com>
2018-05-08 00:14:00 +0300

    Add right left thrust

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    Rigidbody rigidbody;
+
 	// Use this for initialization
 	void Start () {
-		
+        rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -19,7 +21,17 @@ public class Rocket : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
         }
     }
 }

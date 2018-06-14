ccb1fb4f6c7bebc855d6647b774359169e74750c
Klim Agarkov <jewelgatorstv@gmail.com>
2018-06-14 15:14:22 +0300

    Add ingame oscillator for components

---
+++ Assets/Oscillator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector;
+
+    Vector3 startingPos;
+
+    [Range(0, 1)] [SerializeField] float movementFactor;
+
+
+	// Use this for initialization
+	void Start () {
+        startingPos = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+		
+	}
+}
